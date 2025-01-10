5189c9d65227ae0552a746e6891757b7a5b4c9d0
neo06060 <saulcgaos@gmail.com>
2025-01-10 15:56:25 +0100

    a

--- week01/analyze/Search.cs
+++ week01/analyze/Search.cs
@@ -1,65 +1,41 @@
-﻿using System.Diagnostics;
+﻿using System;
 
 public static class Search {
     public static void Run() {
-        Console.WriteLine("{0,15}{1,15}{2,15}{3,15}{4,15}", "n", "sort1-count", "sort2-count", "sort1-time",
-            "sort2-time");
-        Console.WriteLine("{0,15}{0,15}{0,15}{0,15}{0,15}", "----------");
-
-        for (int n = 0; n <= 25000; n += 1000) {
-            var testData = Enumerable.Range(0, n).ToArray();
-            int count1 = SearchSorted1(testData, n);
-            int count2 = SearchSorted2(testData, n, 0, testData.Length - 1);
-            double time1 = Time(() => SearchSorted1(testData, n), 100);
-            double time2 = Time(() => SearchSorted2(testData, n, 0, testData.Length - 1), 100);
-            Console.WriteLine("{0,15}{1,15}{2,15}{3,15:0.00000}{4,15:0.00000}", n, count1, count2, time1, time2);
-        }
+        var sortedArray = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Console.WriteLine("Searching for 5 using SearchSorted1...");
+        int target1 = 5;
+        bool result1 = SearchSorted1(sortedArray, target1);
+        Console.WriteLine(result1 ? $"Found {target1}!" : $"{target1} not found.");
+        Console.WriteLine("Searching for 5 using SearchSorted2...");
+        bool result2 = SearchSorted2(sortedArray, target1);
+        Console.WriteLine(result2 ? $"Found {target1}!" : $"{target1} not found.");
+        int targetNotFound = 11;
+        Console.WriteLine("Searching for 11 using SearchSorted1...");
+        bool resultNotFound1 = SearchSorted1(sortedArray, targetNotFound);
+        Console.WriteLine(resultNotFound1 ? $"Found {targetNotFound}!" : $"{targetNotFound} not found.");
+        Console.WriteLine("Searching for 11 using SearchSorted2...");
+        bool resultNotFound2 = SearchSorted2(sortedArray, targetNotFound);
+        Console.WriteLine(resultNotFound2 ? $"Found {targetNotFound}!" : $"{targetNotFound} not found.");
     }
-
-    private static double Time(Action executeAlgorithm, int times) {
-        var sw = Stopwatch.StartNew();
-        for (var i = 0; i < times; ++i) {
-            executeAlgorithm();
+    public static bool SearchSorted1(int[] sortedArray, int target) {
+        for (int i = 0; i < sortedArray.Length; i++) {
+            if (sortedArray[i] == target)
+                return true;
         }
-
-        sw.Stop();
-        return sw.Elapsed.TotalMilliseconds / times;
+        return false;
     }
-
-    /// <summary>
-    /// Search for 'target' in the list 'data'. When its found (or not found) the variable count which represents
-    /// the work done in the function is returned.
-    /// </summary>
-    /// <param name="data">The array of numbers</param>
-    /// <param name="target">The number we're looking for</param>
-    private static int SearchSorted1(int[] data, int target) {
-        var count = 0;
-        foreach (var item in data) {
-            count += 1;
-            if (item == target)
-                return count; // Found it
+    public static bool SearchSorted2(int[] sortedArray, int target) {
+        int left = 0, right = sortedArray.Length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (sortedArray[mid] == target)
+                return true;
+            else if (sortedArray[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
         }
-
-        return count; // Didn't find it
-    }
-
-    /// <summary>
-    /// Search for 'target' in the list 'data'. When its found (or not found) the variable count which represents
-    /// the work done in the function is returned.
-    /// </summary>
-    /// <param name="data">The array of numbers</param>
-    /// <param name="target">The number we're looking for</param>
-    /// <param name="start">The index of the starting section of the data to look in</param>
-    /// <param name="end">The index of the ending section of the data to look in</param>
-    private static int SearchSorted2(int[] data, int target, int start, int end) {
-        if (end < start)
-            return 1; // All done
-        var middle = (end + start) / 2;
-        if (data[middle] == target)
-            return 1; // Found it
-        if (data[middle] < target) // Search in the upper half after index middle
-            return 1 + SearchSorted2(data, target, middle + 1, end);
-        // Search in the lower half before index middle
-        return 1 + SearchSorted2(data, target, start, middle - 1);
+        return false;
     }
 }
